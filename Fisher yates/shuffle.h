4f91c0af368962bc257b207e7671f9161ecb68a8
ishika kesarwani <imusun24@gmail.com>
2021-09-28 01:12:46 +0530

    fisher yates

--- Fisher yates/shuffle.h
+++ Fisher yates/shuffle.h
@@ -1,6 +1,33 @@
-class Solution{
-     private int[] nums;   // array to be shuffled
-     private Random random; // random number generator
+class Solution
+{
+private
+    int[] nums; // array to be shuffled
+private
+    Random random; // random number generator
 
-     
-}
+public
+    Solution(int[] nums) // constructor
+    {
+        this.nums = nums;           // initialize the array
+        this.random = new Random(); // initialize the random number generator
+    }
+public
+    int[] reset() // reset the array to its original configuration and return it.
+    {
+        return nums;
+    }
+public
+    int[] shuffle() // shuffle the array and return it.
+    {
+        int[] copy = nums.clone();               // clone the array
+        for (int i = nums.legth - 1; i > 0; i--) // loop from the last element to the first
+        {
+            int j = random.nextInt(i + 1); // generate a random number from 0 to i
+            //swap(copy,i,j); // swap the elements
+            int t = copy[i];
+            copy[i] = copy[j];
+            copy[j] = t;
+        }
+        return copy;
+    }
+};
